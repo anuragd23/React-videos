cca25de6377e30e3e263d6624e9637cf13c4d492
Anurag Das <anurag.das23.ad@gmail.com>
2021-01-21 22:27:19 -0500

    Added YouTube API and axios config

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyCtvsvJK0yyOg3Umf3088rGd0hkn-jg2rY';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
