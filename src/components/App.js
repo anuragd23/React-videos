bdc35a59596863c231bf83e1e1ab8f3b5a7580b4
Anurag Das <anurag.das23.ad@gmail.com>
2021-01-23 11:23:45 -0500

    Added video list styling

--- src/components/App.js
+++ src/components/App.js
@@ -27,10 +27,18 @@ class App extends React.Component {
         return(
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
-                <VideoDetail selectedVideo={this.state.selectedVideo}/>
-                <VideoList
-                onVideoSelect={this.onVideoSelect} 
-                videos={this.state.videos}/>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail selectedVideo={this.state.selectedVideo}/>
+                        </div>
+                        <div className="five wide column">
+                            <VideoList
+                            onVideoSelect={this.onVideoSelect} 
+                            videos={this.state.videos}/>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     };
--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -3,9 +3,15 @@ import React from 'react';
 const VideoDetail = ({ selectedVideo }) => { 
     if(!selectedVideo) {
         return <div>Loading...</div>
-    }; 
+    };
+    
+    const videoSrc = `https://www.youtube.com/embed/${selectedVideo.id.videoId}`;
+
     return(
         <div>
+            <div className="ui embed">
+                <iframe title='video player' src={videoSrc}/>
+            </div>
             <div className="ui segment">
                 <h4 className="ui header">{selectedVideo.snippet.title}</h4> 
                 <p>{selectedVideo.snippet.description}</p>
